05b11e379512f4cc1431529c99839614fadc28d1
patz <patofunes@gmail.com>
2024-07-19 09:56:47 +0300

    fixed av size

--- src/pages/Music/Music.jsx
+++ src/pages/Music/Music.jsx
@@ -6,7 +6,7 @@ export default function Music() {
     <div className="main-sound">
         <div className="sound-container">
             <div className="frame-link">
-                <iframe title="PFC" width="100%" height="400" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1800825924&color=%2382d0f5&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
+                <iframe title="PFC" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1800825924&color=%2382d0f5&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
             </div>
         </div>
     </div>
--- src/pages/Visual/Visual.jsx
+++ src/pages/Visual/Visual.jsx
@@ -5,7 +5,7 @@ export default function Visual() {
     <>
     <div className='main-visual'>
         <div className="visual-container">
-            <iframe title="Mental Pipes" src="https://player.vimeo.com/video/487660093?h=f2654fd42e" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+            <iframe title="Mental Pipes" src="https://player.vimeo.com/video/487660093?h=f2654fd42e" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
         </div>
     </div>
     </>

218e72e2e27160a2e4b1e92f110f1bd541c44971
patz <patofunes@gmail.com>
2024-07-31 23:53:12 +0300

    fixed the emailjs

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,36 @@
 import "./Contact.css";
-import { initializeEmailJS, sendMail } from "../../components/Scripts/script";
 import { useEffect } from "react";
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+
+    function initializeEmailJS() {
+        emailjs.init("cpjC4NJP5ymKa_Zz5"); 
+    }
+    
+    function sendMail() {
+        var params = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            message: document.getElementById("message").value
+        };
+    
+    const serviceID = "portfolio_contact_form";
+    const templateID = "template_54b1z2m";
+    
+    emailjs.send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log("Success:", res);
+            alert("Thank you for contacting me! I will get back to you soon.");
+        })
+        .catch((err) => {
+            console.error("Failed to send email:", err);
+            alert("There was an error sending your message. Please try again later.");
+        });
+    }
 
     useEffect(() => {
         initializeEmailJS();

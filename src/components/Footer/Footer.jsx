b5b1de75faa84362d4f156e413aea39b49bb5820
patz <patofunes@gmail.com>
2024-10-30 21:02:08 +0200

    update to icons in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,34 @@ import { Link } from "react-router-dom";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <footer className="footer is-fixed-bottom">
-            <div className="content has-text-centered">
-                <p><strong>Thanks</strong> for stopping by.</p>
-                <p>All rights reserved ® | PFC | 2024</p>
-            </div>
-            <div className="has-text-centered">
-                    <Link to="https://github.com/elpatoenlasolas" alt="link to github" target="_blank"><FaGithub className="fa-icon" /></Link>
-                    <Link to="https://linkedin.com/in/patricio-funes-cardoso" alt="link to linkedin" target="_blank"><FaLinkedin className="fa-icon" /></Link>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer is-fixed-bottom">
+      <div className="content has-text-centered">
+        <p>
+          <strong>Thanks</strong> for stopping by.
+        </p>
+        <p>All rights reserved ® | PFC | 2024</p>
+      </div>
+      <div className="has-text-centered">
+        <Link
+          to="https://github.com/elpatoenlasolas"
+          alt="link to github"
+          target="_blank"
+          className="icons"
+        >
+          <FaGithub />
+        </Link>
+        <Link
+          to="https://linkedin.com/in/patricio-funes-cardoso"
+          alt="link to linkedin"
+          target="_blank"
+          className="icons"
+        >
+          <FaLinkedin />
+        </Link>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

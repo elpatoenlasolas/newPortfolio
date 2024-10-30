55ba214b4f42ffd2857213d94808ef125f36f691
patz <103054522+elpatoenlasolas@users.noreply.github.com>
2024-10-30 16:54:29 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
                 <p><strong>Thanks</strong> for stopping by.</p>
                 <p>All rights reserved ® | PFC | 2024</p>
             </div>
-            <div className="icon has-text-centered">
+            <div className="has-text-centered">
                     <Link to="https://github.com/elpatoenlasolas" alt="link to github" target="_blank"><FaGithub className="fa-icon" /></Link>
                     <Link to="https://linkedin.com/in/patricio-funes-cardoso" alt="link to linkedin" target="_blank"><FaLinkedin className="fa-icon" /></Link>
             </div>
